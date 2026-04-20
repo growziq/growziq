146a1ba2a0718fea114bfe2ad332eb90cfb72605
growziq <dev.growziq@gmail.com>
2026-04-20 22:14:20 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        portfolio: resolve(__dirname, 'portfolio.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        tos: resolve(__dirname, 'tos.html'),
+      },
+    },
+  },
+});
